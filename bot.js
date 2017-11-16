2fb298e123b53bb86d7f321b5ab859eb7821b35a
Jack Chambers <jtcroxley@gmail.com>
2017-11-16 10:23:05 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.user.setGame('Eating Shit!')
+
 client.on('ready', () => {
     console.log('Bot Online');
 });
 
+
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '!ping') {
     	message.channel.send('pong');
   	}
+    if (message.content === '!
 });
 
 // THIS  MUST  BE  THIS  WAY
