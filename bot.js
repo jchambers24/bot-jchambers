d33e44832ff041798063dec1bc0a90beee0fb6b6
Jack Chambers <jtcroxley@gmail.com>
2017-11-17 08:23:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "!"
 
 client.on('ready', () => { 
     client.log("Ready!")
 });
 
 
-client.on('message', msg => {
-       if (msg.content == "!ping") {
-           msg.channel.send("pong");
-       };    
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
            
 });
 
