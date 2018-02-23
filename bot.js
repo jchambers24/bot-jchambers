cb82b4948d3af377eb4a783f8f47d4ce1a3ff4cd
Jack Chambers <jtcroxley@gmail.com>
2018-02-23 10:25:53 -0500

    fixed!

--- bot.js
+++ bot.js
@@ -3,16 +3,14 @@ const client = new Discord.Client();
 const prefix = "+"
 const version = "1.0"
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`); // console login message
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => { // simple command
-  message.channel.send("RocketBot Online"+)
+client.on('message', message => {
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
+  const command = args.shift().toLowerCase();
  
-  
   if(command == 'ping') {
   message.channel.send('Pong!');
   } else
