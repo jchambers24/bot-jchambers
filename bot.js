29d3a76c99fb098fbf55ca67ed3b27731a923d12
Jack Chambers <jtcroxley@gmail.com>
2018-02-23 10:39:36 -0500

    Added Kick and Ping

--- bot.js
+++ bot.js
@@ -11,11 +11,20 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
  
-  if(command == 'announcement') {
-    message.channel.send("@everyone"+args)
+  if(command == 'announcement') { // Test Announcement
+    message.channel.send("@everyone"+args);
   } else
-  if (command == 'ping') {
-  message.channel.reply('Pong!');
+ 
+   if (command == 'ping') { // Test Command
+    message.channel.reply('Pong!');
+  
+   if (command == 'kick') { // Kick A Player
+   player = message.mentions.members.first(); 
+        player.kick().then((member) => { //Succes
+            message.channel.send(":wave: " + player.displayName + " has been kicked");
+        }).catch(() => { // Failed or Bad Perms
+            message.channel.send("Access Denied :wink: Nice Try" + message.author.displayName);
+  }
 }
 
 });
